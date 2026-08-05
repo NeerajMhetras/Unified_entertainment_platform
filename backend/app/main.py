da1069bce027796db1159a6aecda97c035c41206
Neeraj Mhetras <neerajmhetras@gmail.com>
2026-08-05 22:32:33 +0530

    conneccted postgre with backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.database.database import engine, Base
+from app.models.user import User
+
 
 app = FastAPI(
     title = "Unified Entertainment Platform API",
     version = "1.0.0",
 )
 
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Unified Entertainment Platform API!"}
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        return {"message": "API is working!", "database_connection": "successful"}
+    except Exception as e:
+        return {"error":str(e)}
+
