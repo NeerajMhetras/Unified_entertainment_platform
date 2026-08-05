99b9e95c78de11f221783d7cb21379675c449ae3
Neeraj Mhetras <neerajmhetras@gmail.com>
2026-08-06 00:12:19 +0530

    added password hashing functionality

--- backend/app/api/routers/user.py
+++ backend/app/api/routers/user.py
@@ -1,8 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+
 from app.schemas.user import UserCreate
+from app.database.dependencies import get_db
+from app.models.user import User
+from app.core.security import hash_password
+
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/")
-async def create_user(user: UserCreate):
-    return user
+async def create_user(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password_hash=hash_password(user.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {
+        "id": new_user.id,
+        "username": new_user.username,
+        "email": new_user.email,
+    }
